954bc3e4e3a5d96c2767d28696fe516b9c735ac9
tan jiaming <2247854466@qq.com>
2024-01-12 17:15:04 +0800

    lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node* q;
+    node* p;
+    p = head;
+    if(head==NULL)
+    	return 0;
+
+    q=p->next;
+    if(q==NULL)
+    	return 0;
+    while(p!=q){
+        if(q->next!=NULL && q->next->next!=NULL)
+        	q=q->next->next;
+        else{
+        	return 0;
+        }
+        p=p->next;
+    }
+
+    return 1;
 }
